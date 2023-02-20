86c5418eead016c795dd83818961929879e1d8c7
Rahul Parihar <rahul.parihar@jalantechnologies.com>
2023-02-20 15:46:17 +0530

    formatted code with prettier

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
 const userScore = 0;
 const compScore = 0;
 const obj = {
-    0: "rock",
-    1: "paper",
-    2: "scissors"
+  0: "rock",
+  1: "paper",
+  2: "scissors",
+};
+function getComputerChoice() {
+  const rand = Math.floor(Math.random() * 3);
+  return obj[rand];
 }
-function getComputerChoice(){
-    const rand = Math.floor(Math.random()*3)
-    return obj[rand]
+function runRound(userChoice) {
+  console.log("Player -> " + userChoice);
+  console.log("Computer -> " + getComputerChoice());
 }
-function runRound(userChoice){
-    console.log("Player -> "+ userChoice)
-    console.log("Computer -> "+getComputerChoice())
-}
-document.addEventListener('click',(e)=>{
-    if(e.target.classList.contains("rock"))
-        runRound("rock")
-    else if(e.target.classList.contains("paper"))
-        runRound("paper")
-    else if(e.target.classList.contains("scissors"))
-        runRound("scissors") 
-})
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("rock")) runRound("rock");
+  else if (e.target.classList.contains("paper")) runRound("paper");
+  else if (e.target.classList.contains("scissors")) runRound("scissors");
+});
