8e18af90810259764f6b1f61359abadc37323dd9
Rahul Parihar <rahul.parihar@jalantechnologies.com>
2023-02-20 15:20:50 +0530

    function to get Choices

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-console.log("linked")
-console.log("development code")
+const userScore = 0;
+const compScore = 0;
+const obj = {
+    0: "rock",
+    1: "paper",
+    2: "scissors"
+}
+function getComputerChoice(){
+    const rand = Math.floor(Math.random()*3)
+    return obj[rand]
+}
+function runRound(userChoice){
+    console.log("Player -> "+ userChoice)
+    console.log("Computer -> "+getComputerChoice())
+}
+document.addEventListener('click',(e)=>{
+    if(e.target.classList.contains("rock"))
+        runRound("rock")
+    else if(e.target.classList.contains("paper"))
+        runRound("paper")
+    else if(e.target.classList.contains("scissors"))
+        runRound("scissors") 
+})
